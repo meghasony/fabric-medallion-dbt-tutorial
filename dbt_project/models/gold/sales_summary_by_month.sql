b7df20621b16b00d880a5b628b170a1c21a26fc2
Megha Sony <meghasony1997@gmail.com>
2026-06-29 20:02:45 +0530

    Update sales_summary_by_month.sql

--- dbt_project/models/gold/sales_summary_by_month.sql
+++ dbt_project/models/gold/sales_summary_by_month.sql
@@ -1,18 +1,19 @@
-{{ config(materialized='table', tags=['gold', 'powerbi']) }}
+{{ config(materialized='table', tags=['gold']) }}
 
-SELECT
+select
     d.year_month,
     f.channel,
     p.category,
-    COUNT(DISTINCT f.order_id) AS order_count,
-    SUM(f.quantity) AS units_sold,
-    SUM(f.net_sales_amount) AS net_sales_amount,
-    SUM(f.gross_margin_amount) AS gross_margin_amount,
-    SUM(f.returned_quantity) AS returned_quantity,
-    SUM(f.returned_sales_amount) AS returned_sales_amount
-FROM {{ ref('fact_sales') }} f
-INNER JOIN {{ ref('dim_date') }} d
-    ON f.date_key = d.date_key
-INNER JOIN {{ ref('dim_product') }} p
-    ON f.product_id = p.product_id
+    count(distinct f.order_id) as orders,
+    sum(f.quantity) as units_sold,
+    sum(f.net_sales_amount) as net_sales_amount,
+    sum(f.gross_margin_amount) as gross_margin_amount,
+    sum(f.returned_quantity) as returned_quantity
+
+from {{ ref('fact_sales') }} f
+join {{ ref('dim_date') }} d
+    on f.date_key = d.date_key
+join {{ ref('dim_product') }} p
+    on f.product_id = p.product_id
+
 {{ dbt_utils.group_by(n=3) }}
